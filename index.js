d02aa1fccf63b842c0e9a92486681e6c3324eb91
arne <arne@mintlab.nl>
2017-10-20 11:50:55 +0200

    Add get for route index

--- index.js
+++ index.js
@@ -12,8 +12,11 @@ const pool = new Pool({
       password: process.env.POSTGRES_PASSWORD
 });
 const SQL = require('sql-template-strings');
+const request = require('request');
+const baseUrl = 'http://localhost:3000';
 
 app.set('view engine', 'ejs');
+app.engine('ejs', require('express-ejs-extend'));
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded());
@@ -22,80 +25,63 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 require('./server/routes')(app);
 
-app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the beginning of nothingness.',
-}));
+app.route('/')
+    .get(function (req, res) {
+        request(`${baseUrl}/api/post/all`, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                res.render('./pages/index', {posts: JSON.parse(body)});
+            }
+        });
+    });
 
-// app.route('/')
-//     .get(function (req, res) {
-//         // if (loggedin) {
-//             res.render('./pages/index', {posts:
-//                 [
-//                     {
-//                         id: '1',
-//                         title: 'This is my first post',
-//                         author: 'arne'
-//                     },
-//                     {
-//                         id: '2',
-//                         title: 'This is my second post',
-//                         author: 'lloyd'
-//                     }
-//                 ]
-//             });
-//         // } else {
-//         //     res.redirect('./pages/login');
-//         // }
-//     });
+app.route('/login')
+    .get(function (req, res) {
+        res.render('./pages/login');
+    })
+    .post(function (req, res) {
+        // login();
+        res.redirect('/');
+    });
 
-// app.route('/login')
-//     .get(function (req, res) {
-//         res.render('./pages/login');
-//     })
-//     .post(function (req, res) {
-//         // login();
-//         res.redirect('./pages/index');
-//     });
+app.route('/new')
+    .get(function (req, res) {
+        // if (loggedin) {
+            res.render('./pages/new');
+        // } else {
+        //     res.redirect('login');
+        // }
+    })
+    .post(function (req, res) {
+        // savePost();
+        res.redirect('posts');
+    });
 
-// app.route('/new')
-//     .get(function (req, res) {
-//         // if (loggedin) {
-//             res.render('./pages/new');
-//         // } else {
-//         //     res.redirect('./pages/login');
-//         // }
-//     })
-//     .post(function (req, res) {
-//         // savePost();
-//         res.redirect('posts');
-//     });
+app.route('/register')
+    .get(function (req, res) {
+        res.render('./pages/register');
+    })
+    .post(function (req, res) {
+        // saveUser
+        res.redirect('login');
+    });
 
-// app.route('/register')
-//     .get(function (req, res) {
-//         res.render('./pages/register');
-//     })
-//     .post(function (req, res) {
-//         // saveUser
-//         res.redirect('login');
-//     });
+app.route('/logout')
+    .get(function (req, res) {
+        // logout()
+        res.redirect('login');
+    });
 
-// app.route('/logout')
-//     .get(function (req, res) {
-//         // logout()
-//         res.redirect('login');
-//     });
-
-// app.route('/post')
-//     .get(function (req, res) {
-//         // if (loggedin) {
-//             res.render('./pages/posts');
-//         // } else {
-//         //     res.redirect('./pages/login');
-//         // }
-//     })
-//     .post(function (req, res) {
-//         // saveComment();
-//         res.redirect('posts');
-//     });
+app.route('/post')
+    .get(function (req, res) {
+        // if (loggedin) {
+            res.render('./pages/posts');
+        // } else {
+        //     res.redirect('login');
+        // }
+    })
+    .post(function (req, res) {
+        // saveComment();
+        res.redirect('posts');
+    });
 
 module.exports = app;
