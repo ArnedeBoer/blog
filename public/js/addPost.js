d3c5b0965d57d06c01a2eb65343c74d1b158bf05
arne <arne@mintlab.nl>
2017-10-24 12:08:41 +0200

    Make post creater the post author

--- public/js/addPost.js
+++ public/js/addPost.js
@@ -1,17 +1,17 @@
 $('#new-post #submit').click(function() {
     event.preventDefault();
 
-    const userid = '1';
-
-    $.ajax({
-        type: 'POST',
-        url: `/api/user/${userid}/post`,
-        data: {
-            title: $("#title").val(),
-            body: $("#body").val()
-        },
-        success: function(newPost) {
-            window.location.replace(`/post/?post=${newPost.id}`);
-        }
+    $.get('/retrieveUserid', function(data) {
+        $.ajax({
+            type: 'POST',
+            url: `/api/user/${data.userid}/post`,
+            data: {
+                title: $("#title").val(),
+                body: $("#body").val()
+            },
+            success: function(newPost) {
+                window.location.replace(`/post/?post=${newPost.id}`);
+            }
+        });
     });
 });
